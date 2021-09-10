d9dfe1c62a43bc24b7553586e08bd535658e08ef
saifaldin14 <saifaldindeveloper@gmail.com>
2021-09-10 17:13:42 -0400

    Gradient color for title and fix shearable link pop up

--- src/components/ShareableLink/index.js
+++ src/components/ShareableLink/index.js
@@ -1,9 +1,20 @@
-import { Dialog, DialogTitle } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ShareableLink = (props) => {
   const { onClose, selectedValue, open, title, imageUrl } = props;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleClose = () => {
     onClose(selectedValue);
@@ -14,23 +25,43 @@ const ShareableLink = (props) => {
   }, [title, imageUrl]);
 
   return (
-    <Dialog onClose={handleClose} aria-labelledby="shareable-link" open={open}>
+    <Dialog
+      fullScreen={fullScreen}
+      onClose={handleClose}
+      aria-labelledby="shareable-link"
+      open={open}
+    >
       <DialogTitle id="image-title">
         Access Shareable Link for the Image
       </DialogTitle>
-      <Link
-        to={{
-          pathname: `/${title}`,
-          state: {
-            imageUrl: `${imageUrl}`,
-            title: `${title}`,
-          },
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {`${window.location.href}${title}`}
-      </Link>
+      <DialogContent>
+        <DialogContentText>
+          {`${window.location.href}${title}`}
+          Let Google help apps determine location. This means sending anonymous
+          location data to Google, even when no apps are running.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={handleClose} color="primary">
+          <Link
+            to={{
+              pathname: `/${title}`,
+              state: {
+                imageUrl: `${imageUrl}`,
+                title: `${title}`,
+              },
+            }}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            Visit Link
+          </Link>
+        </Button>
+        <Button onClick={handleClose} color="primary" autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
